7a85cf0675c90a6714d994ecb247127ba9c36ae8
André Guilherme <andre.guilherme2009@gmail.com>
2024-03-25 21:06:57 -0300

    Atualização

--- Sistema-Bancario-C++/Sistema-Bancario-C++/Sistema-Bancario-C++.cpp
+++ Sistema-Bancario-C++/Sistema-Bancario-C++/Sistema-Bancario-C++.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string> 
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -23,7 +23,7 @@ int main() {
 
     vector<Client> usuarios;
 
-    cout << "Bem vindo ao sistema bancário!" << endl;
+    cout << "Bem vindo ao sistema bancario!" << endl;
     cout << "Primeiramente vamos realizar seu cadastro: " << endl;
     cout << "Informe seu nome completo: ";
     getline(cin, name);
@@ -41,12 +41,12 @@ int main() {
     cout << "Usuario cadastrado com sucesso!\nO que você deseja fazer agora?" << endl;
 
     do {
-        cout << "1-Cadastrar novo usuario\n2-Editar usuario\n3-Excluir usuario" << endl;
+        cout << "1-Cadastrar novo usuario\n2-Editar usuario\n3-Excluir usuario\n4-Sair" << endl;
         cin >> selecao;
 
         switch (selecao) {
         case 1: {
-            cin.ignore(); 
+            cin.ignore();
             cout << "Informe seu nome completo: ";
             getline(cin, name);
 
@@ -58,26 +58,64 @@ int main() {
 
             Client newClient(name, senha, email);
 
-            usuarios.push_back(newClient); 
+            usuarios.push_back(newClient);
             break;
         }
         case 2: {
             string nameEdit;
-            cout << "Para qual usuário voce deseja editar? Digite o nome: ";
+            cout << "Para qual usuario voce deseja editar? Digite o nome: ";
+            cin.ignore();
             getline(cin, nameEdit);
 
             bool encontrado = false;
-            for (const auto& cliente : usuarios) {
+            for (auto& cliente : usuarios) {
                 if (cliente.nome == nameEdit) {
                     encontrado = true;
+                    
+                    cout << "\nInformacoes " << endl;
+
+                    cout << "\nInforme seu nome completo: ";
+                    getline(cin, name);
+
+                    cout << "\nAgora crie uma senha: ";
+                    cin >> senha;
+
+                    cout << "\nInforme o email: ";
+                    cin >> email;
+
+                    cliente.nome = name;
+                    cliente.senha = senha;
+                    cliente.email = email;
+
+                    break; 
                 }
-                break;
             }
+            if (!encontrado)
+                cout << "Usuário nao encontrado!" << endl;
+            else
+                cout << "Usuário editado com sucesso!" << endl;
+            break;
+        }
+        case 3: {
+            string nameDelete;
+            cout << "Qual usuário você deseja excluir? Digite o nome: ";
+            cin.ignore();
+            getline(cin, nameDelete);
 
-            cout << "Usuário removido com sucesso!" << endl;
-
+            for (auto it = usuarios.begin(); it != usuarios.end(); ++it) {
+                if (it->nome == nameDelete) {
+                    usuarios.erase(it); 
+                    cout << "Usuário removido com sucesso!" << endl;
+                    break; 
+                }
+            }
+            break;
+        }
+        case 4:
+            cout << "Saindo do programa..." << endl;
+            break;
         default:
-            cout << "Opçao inválida!" << endl;
+            cout << "Opção inválida!" << endl;
             break;
         }
     } while (selecao != 4);
